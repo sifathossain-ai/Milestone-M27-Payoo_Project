e30c2cad2962c047709910246132a09771b02917
Sifat Hossain <sifathossain1326@gmail.com>
2025-07-21 01:03:07 +0530

    Add Money

--- js/home.js
+++ js/home.js
@@ -1,1 +1,21 @@
-console.log("Hello Add Mony")
+
+document.getElementById('btn-add-money').addEventListener('click', function () {
+
+
+    const getMoney = document.getElementById('input-money').value;
+    const getPin = document.getElementById('input-pin').value;
+
+    if (getPin === '1111') {
+        console.log("Money successfully added.");
+
+        const getBalance = document.getElementById('balance').innerText;
+
+        const totalBalance = parseInt(getBalance) + parseInt(getMoney);
+        document.getElementById('balance').innerText = totalBalance;
+
+    }
+    else {
+        alert('Fail to add money.');
+    }
+    // console.log(getMoney, getPin);
+})
